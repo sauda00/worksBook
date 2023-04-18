bd26c1bc1c2fbe70305c610463d9032744b6af57
sauda00 <saudakalybekova@gmil.come>
2023-04-18 15:54:08 +0600

    school

---
+++ src/components/Home/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Index = () => {
+    return (
+        <div>
+            <h3>cvbn</h3>
+        </div>
+    );
+};
+
+export default Index;
---
+++ src/components/YourCart/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Index = () => {
+    return (
+        <div>
+            <h4>fghbjnkl</h4>
+        </div>
+    );
+};
+
+export default Index;
