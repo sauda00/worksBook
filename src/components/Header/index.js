b52a5808ea5ae444750ab6912d567c00c5891daa
Your Name <san40.myrzakmatov@gmail.com>
2023-04-20 15:19:49 +0600

    book

---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className='h1'>adsvcsdvs</h1>
+        </div>
+    );
+};
+
+export default Header;
