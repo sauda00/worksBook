579f2bb924bfc42afafa3ef11c1cab47c9d335d1
sauda00 <saudakalybekova@gmil.come>
2023-04-25 20:03:25 +0600

    changes

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import {BsFacebook, BsInstagram, BsLinkedin, BsTelegram, BsTwitter, BsWhatsapp} from "react-icons/bs";
+import {NavLink} from "react-router-dom";
 
 const Footer = () => {
     return (
@@ -10,27 +12,35 @@ const Footer = () => {
                      <p>Be the first to know about our special offers, news, and updates.</p>
                      <input type="text" placeholder="Email Address"/>
                  </div>
-                 <div className="footer--nav2">
-                     <h3>Lorem Ipsum</h3>
-                     <p>lorem</p>
-                     <p>lorem</p>
-                     <p>lorem</p>
-                     <p>lorem</p>
-                 </div>
-                 <div className="footer--nav2">
-                     <h3>Lorem Ipsum</h3>
-                     <p>lorem</p>
-                     <p>lorem</p>
-                     <p>lorem</p>
-                     <p>lorem</p>
-                 </div>
-                 <div className="footer--nav2">
-                     <h3>Lorem Ipsum</h3>
-                     <p>lorem</p>
-                     <p>lorem</p>
-                     <p>lorem</p>
-                     <p>lorem</p>
-                 </div>
+
+                <div className="footer--nav2">
+
+                    <div className="footer--nav2__nav3">
+                        <NavLink to={'/'}>Bookshop</NavLink>
+                        <NavLink to={'/categories'}>Categories</NavLink>
+                        <NavLink to={'/recent'}>Recent</NavLink>
+                        <NavLink to={'/allBooks'}>Books</NavLink>
+                        <NavLink to={'/useBook'}>About Us</NavLink>
+                    </div>
+
+                    <div className="footer--nav2__nav4">
+                        <div className="footer--nav2__nav4--tel">
+                            <h1>contacts</h1>
+                            <div><BsWhatsapp className="footer--nav2__nav4--tel__vk"/><a href="tel:+996">0700 232 400</a></div>
+                            <div><BsWhatsapp className="footer--nav2__nav4--tel__vk"/><a href="tel:+996">0773 400 551</a></div>
+                        </div>
+                        <div className='footer--nav2__nav4--in'>
+                            <a href="https://www.instagram.com/motion_web/"><BsInstagram/></a>
+                            <a href="#"><BsLinkedin/></a>
+                            <a href="#"><BsTwitter/></a>
+                            <a href="#"><BsFacebook/></a>
+                            <a href="#"><BsTelegram/></a>
+                        </div>
+                    </div>
+                </div>
+
+
+
              </div>
          </div>
         </div>
