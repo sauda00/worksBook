a25cff5dbb5833ac0cd56e9b656aacd79134bc88
Baielemilbekov <130228856+Baielemilbekov@users.noreply.github.com>
2023-04-30 09:32:52 +0300

    card (#9)

--- src/components/Home/NewBooks/index.js
+++ src/components/Home/NewBooks/index.js
@@ -41,7 +41,6 @@ const NewBooks = () => {
                                             <div className="newBooks--books__card">
                                                 <img width={200} src={img} alt=""/>
                                                 <h3>{el.volumeInfo.title}</h3>
-
                                             </div>
                                         </>
                                     )
